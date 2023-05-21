3ee627f1999166696021b2124227a9fd166166c6
ljki1234 <jabikihind@gmail.com>
2023-05-21 13:47:03 +0200

    add commande attribute

--- gestionCommercial/src/app/controller/model/commande.model.ts
+++ gestionCommercial/src/app/controller/model/commande.model.ts
@@ -1,5 +1,39 @@
 export class Commande {
-  private id:number |undefined ;
-  private ref:string |undefined ;
-  private total:number |undefined ;
+
+  private _id:number |undefined ;
+  private _ref:string |undefined ;
+  private _total:number |undefined ;
+  private _totalePaye:number |undefined ;
+  get id(): number | undefined {
+    return this._id;
+  }
+
+  set id(value: number | undefined) {
+    this._id = value;
+  }
+
+  get ref(): string | undefined {
+    return this._ref;
+  }
+
+  set ref(value: string | undefined) {
+    this._ref = value;
+  }
+
+  get total(): number | undefined {
+    return this._total;
+  }
+
+  set total(value: number | undefined) {
+    this._total = value;
+  }
+
+  get totalePaye(): number | undefined {
+    return this._totalePaye;
+  }
+
+  set totalePaye(value: number | undefined) {
+    this._totalePaye = value;
+  }
+
 }
