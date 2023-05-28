836f70e4068ffb4803da897b4bffbb7c17eaaed5
ljki1234 <jabikihind@gmail.com>
2023-05-28 12:49:22 +0200

    file back

---
+++ gestion-cmd-back/src/test/java/com/example/gestioncmdback/GestionCmdBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestioncmdback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCmdBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
